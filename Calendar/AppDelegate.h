867c8469ba3c980e7b440859236cdd1030c9ba36
Angelina Zhang <azhang7740@gmail.com>
2022-07-05 10:59:51 -0700

    Initial Commit

---
+++ Calendar/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Calendar
+//
+//  Created by Angelina Zhang on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Calendar/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Calendar
+//
+//  Created by Angelina Zhang on 7/5/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
