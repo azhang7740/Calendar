81166f80fa3a8215f41853d5fa15e49407f0c3ca
Angelina Zhang <azhang7740@gmail.com>
2022-07-07 10:10:09 -0700

    Added ParseEvent models

---
+++ Calendar/Models/Event.h
@@ -0,0 +1,16 @@
+//
+//  Event.h
+//  Calendar
+//
+//  Created by Angelina Zhang on 7/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Event : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
