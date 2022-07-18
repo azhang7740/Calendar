644424ffcffcd9c882dbb5afa3d67dcf0d06a085
Angelina Zhang <azhang7740@gmail.com>
2022-07-18 11:13:39 -0700

    Refactored CalendarKit event handling into separate class

--- CalendarApp/View Controllers/DailyCalendarViewController.swift
+++ CalendarApp/View Controllers/DailyCalendarViewController.swift
@@ -19,68 +19,31 @@ public protocol EventInteraction {
 class DailyCalendarViewController : DayViewController {
     
     var controllerDelegate: EventInteraction?
-    private var dateToCalendarKitEvents = [Date: [CalendarKit.Event]]()
+    private let calendarEventHandler = CalendarEventHandler()
     
     func updateCalendarEvent(_ event: CalendarApp.Event, _ date: Date) {
-        guard var calendarKitEvents = dateToCalendarKitEvents[date] else {
-            return
-        }
-        let eventIndex = getEventIndex(event.objectUUID, calendarKitEvents)
-        calendarKitEvents[eventIndex] = getCalendarKitEvent(event)
-        dateToCalendarKitEvents[date] = calendarKitEvents
+        calendarEventHandler.updateEvent(event, date)
         reloadData()
     }
     
     func deleteCalendarEvent(_ event: CalendarApp.Event, _ date: Date) {
-        guard var calendarKitEvents = dateToCalendarKitEvents[date] else {
-            return
-        }
-        let eventIndex = getEventIndex(event.objectUUID, calendarKitEvents)
-        calendarKitEvents.remove(at: eventIndex)
-        dateToCalendarKitEvents[date] = calendarKitEvents;
+        calendarEventHandler.deleteEvent(event, date)
         reloadData()
-    }
-    
-    func getEventIndex(_ eventID: UUID, _ calendarKitEvents: [CalendarKit.Event]) -> Int {
-        for eventIndex in 0...calendarKitEvents.count - 1 {
-            if calendarKitEvents[eventIndex].objectID == eventID {
-                return eventIndex
-            }
-        }
-        return -1
     }
     
     func failedRequest(_ errorMessage: String) {
         // TODO: Error handling
     }
     
-    func addCalendarKitEventFromEvents(_ events:[CalendarApp.Event], _ date: Date) {
-        for eventModel in events {
-            dateToCalendarKitEvents[date]?.append(getCalendarKitEvent(eventModel))
-        }
-    }
-    
     func addEvent(_ eventModel: CalendarApp.Event, _ date: Date) {
-        dateToCalendarKitEvents[date]?.append(getCalendarKitEvent(eventModel))
+        calendarEventHandler.addEvent(eventModel, date)
         reloadData()
-    }
-    
-    func getCalendarKitEvent(_ eventModel: CalendarApp.Event) -> CalendarKit.Event {
-        let dateIntervalFormatter = DateIntervalFormatter()
-        let newEvent = CalendarKit.Event()
-        newEvent.dateInterval = DateInterval(start: eventModel.startDate, end: eventModel.endDate)
-        
-        var info = [eventModel.eventTitle, eventModel.location]
-        info.append(dateIntervalFormatter.string(from: newEvent.dateInterval.start, to: newEvent.dateInterval.end))
-        newEvent.text = info.reduce("", {$0 + $1 + "\n"})
-        newEvent.objectID = eventModel.objectUUID
-        return newEvent
     }
     
     func fetchCalendarEventsForDate(_ date: Date) {
         controllerDelegate?.fetchEventsForDate(date, callback: { events, errorMessage in
             if let newEvents = events {
-                self.addCalendarKitEventFromEvents(newEvents, date)
+                self.calendarEventHandler.addEventsFromArray(newEvents, date)
                 DispatchQueue.main.async {
                     self.reloadData()
                 }
@@ -93,10 +56,9 @@ class DailyCalendarViewController : DayViewController {
     }
     
     override func eventsForDate(_ date: Date) -> [EventDescriptor] {
-        guard let calendarKitEvents = dateToCalendarKitEvents[date] else {
-            dateToCalendarKitEvents[date] = [CalendarKit.Event]()
+        guard let calendarKitEvents = calendarEventHandler.getEventsForDate(date) else {
             fetchCalendarEventsForDate(date)
-            return dateToCalendarKitEvents[date] ?? [ ]
+            return calendarEventHandler.getEventsForDate(date) ?? []
         }
         return calendarKitEvents
     }
