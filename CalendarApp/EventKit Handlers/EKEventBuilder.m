53d9261b4ca39994de3fbdf97c200a6d4514f691
Angelina Zhang <azhang7740@gmail.com>
2022-07-19 13:23:49 -0700

    Implemented data checks for EKEvent conversion

--- CalendarApp/EventKit Handlers/EKEventBuilder.m
+++ CalendarApp/EventKit Handlers/EKEventBuilder.m
@@ -11,6 +11,53 @@
 
 - (Event *)getEventFromEKEvent:(EKEvent *)ekEvent {
     Event *canonicalEvent = [[Event alloc] init];
+    if (!ekEvent.eventIdentifier ||
+        !ekEvent.lastModifiedDate ||
+        !ekEvent.creationDate) {
+        return nil;
+    }
+    
+    canonicalEvent.ekEventID = ekEvent.eventIdentifier;
+    canonicalEvent.updatedAt = ekEvent.lastModifiedDate;
+    canonicalEvent.createdAt = ekEvent.creationDate;
+    
+    if (!ekEvent.title ||
+        ekEvent.title.length == 0) {
+        return nil;
+    }
+    canonicalEvent.eventTitle = ekEvent.title;
+    
+    if (!ekEvent.organizer ||
+        !ekEvent.organizer.name ||
+        ekEvent.organizer.name.length == 0) {
+        canonicalEvent.authorUsername = @"";
+    } else {
+        canonicalEvent.authorUsername = ekEvent.organizer.name;
+    }
+    
+    if (!ekEvent.notes ||
+        ekEvent.notes.length == 0) {
+        canonicalEvent.eventDescription = @"";
+    } else {
+        canonicalEvent.eventDescription = ekEvent.notes;
+    }
+    
+    if (!ekEvent.location ||
+        ekEvent.location.length == 0) {
+        canonicalEvent.location = @"";
+    } else {
+        canonicalEvent.location = ekEvent.location;
+    }
+    
+    if (!ekEvent.startDate ||
+        !ekEvent.endDate ||
+        [ekEvent.startDate compare:ekEvent.endDate] != NSOrderedAscending) {
+        return nil;
+    }
+    canonicalEvent.startDate = ekEvent.startDate;
+    canonicalEvent.endDate = ekEvent.endDate;
+    canonicalEvent.isAllDay = ekEvent.isAllDay;
+    canonicalEvent.objectUUID = [[NSUUID alloc] init];
     
     return canonicalEvent;
 }
