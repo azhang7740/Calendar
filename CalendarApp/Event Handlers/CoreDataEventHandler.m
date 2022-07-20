0b8fbdfb75ca41b60285cfcb2b21060bc1a9990e
Angelina Zhang <azhang7740@gmail.com>
2022-07-20 11:58:54 -0700

    Created CoreDataEventHandler

---
+++ CalendarApp/Event Handlers/CoreDataEventHandler.m
@@ -0,0 +1,46 @@
+//
+//  CoreDataEventHandler.m
+//  CalendarApp
+//
+//  Created by Angelina Zhang on 7/20/22.
+//
+
+#import "CoreDataEventHandler.h"
+#import "AppDelegate.h"
+
+@interface CoreDataEventHandler ()
+
+@property (nonatomic) NSManagedObjectContext *context;
+
+@end
+
+@implementation CoreDataEventHandler
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.context = ((AppDelegate *)UIApplication.sharedApplication.delegate).persistentContainer.viewContext;
+    }
+    return self;
+}
+
+- (void)deleteEvent:(nonnull Event *)event
+         completion:(void (^ _Nonnull)(NSString * _Nullable))completion {
+    
+}
+
+- (void)queryEventsOnDate:(nonnull NSDate *)date
+               completion:(void (^ _Nonnull)(NSMutableArray<Event *> * _Nullable, NSDate * _Nonnull, NSString * _Nullable))completion {
+    
+}
+
+- (void)updateEvent:(nonnull Event *)event
+         completion:(void (^ _Nonnull)(NSString * _Nullable))completion {
+    
+}
+
+- (void)uploadWithEvent:(nonnull Event *)newEvent
+             completion:(void (^ _Nonnull)(Event * _Nonnull, NSDate * _Nonnull, NSString * _Nullable))completion {
+    
+}
+
+@end
