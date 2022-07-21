ac97c2b51f4cc47361c71b73587325753ccdcd83
Angelina Zhang <azhang7740@gmail.com>
2022-07-21 09:13:47 -0700

    Implemented CoreDataEventBuilder

--- CalendarApp/Event Builders/CoreDataEventBuilder.m
+++ CalendarApp/Event Builders/CoreDataEventBuilder.m
@@ -11,6 +11,38 @@
 
 - (Event *)getEventFromCoreDataEvent:(CoreDataEvent *)coreDataEvent {
     Event *canonicalEvent = [[Event alloc] init];
+    if (!coreDataEvent.createdAt ||
+        !coreDataEvent.updatedAt) {
+        return nil;
+    }
+    canonicalEvent.parseObjectId = coreDataEvent.parseID;
+    canonicalEvent.authorUsername = coreDataEvent.authorUsername;
+    canonicalEvent.createdAt = coreDataEvent.createdAt;
+    canonicalEvent.updatedAt = coreDataEvent.updatedAt;
+    
+    if (!coreDataEvent.objectUUID ||
+        [coreDataEvent.objectUUID isEqual:[NSNull null]] ||
+        ![coreDataEvent.objectUUID isKindOfClass:NSUUID.class]) {
+        return nil;
+    }
+    canonicalEvent.objectUUID = coreDataEvent.objectUUID;
+    
+    if (!coreDataEvent.eventTitle ||
+        [coreDataEvent.eventTitle isEqual:[NSNull null]] ||
+        coreDataEvent.eventTitle.length == 0) {
+        return nil;
+    }
+    canonicalEvent.eventTitle = coreDataEvent.eventTitle;
+    canonicalEvent.eventDescription = coreDataEvent.eventDescription;
+    canonicalEvent.location = coreDataEvent.location;
+    
+    if (!coreDataEvent.startDate ||
+        !coreDataEvent.endDate ||
+        [coreDataEvent.startDate compare:coreDataEvent.endDate] != NSOrderedAscending) {
+        return nil;
+    }
+    canonicalEvent.startDate = coreDataEvent.startDate;
+    canonicalEvent.endDate = coreDataEvent.endDate;
     return canonicalEvent;
 }
 
