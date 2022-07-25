58a1cf7030d528baf7dd6e38fd3497a6ab6b639a
Angelina Zhang <azhang7740@gmail.com>
2022-07-25 11:16:24 -0700

    Created LocalChange objects in CoreData

---
+++ CalendarApp/Models/LocalChange+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  LocalChange+CoreDataClass.swift
+//  
+//
+//  Created by Angelina Zhang on 7/25/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(LocalChange)
+public class LocalChange: NSManagedObject {
+
+}
---
+++ CalendarApp/Models/LocalChange+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  LocalChange+CoreDataProperties.swift
+//  
+//
+//  Created by Angelina Zhang on 7/25/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension LocalChange {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LocalChange> {
+        return NSFetchRequest<LocalChange>(entityName: "LocalChange")
+    }
+
+    @objc public enum ChangeType: Int32 {
+        case Delete = 0
+        case Create = 1
+        case Update = 2
+    }
+    
+    @NSManaged public var changeType: ChangeType
+    @NSManaged public var eventUUID: UUID?
+    @NSManaged public var eventParseID: String?
+
+}
