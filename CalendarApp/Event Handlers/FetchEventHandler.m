a074e93f997bbdbc18d1dd84915695dc6d63cfe8
Angelina Zhang <azhang7740@gmail.com>
2022-07-25 11:10:05 -0700

    Created skeleton for new protocol for fetching events

--- CalendarApp/Event Handlers/FetchEventHandler.m
+++ CalendarApp/Event Handlers/FetchEventHandler.m
@@ -9,10 +9,12 @@
 #import "CoreDataEventHandler.h"
 #import "ParseEventHandler.h"
 #import "EventSyncHandler.h"
+#import "CoreDataEventHandler.h"
 
 @interface FetchEventHandler ()
 
 @property (nonatomic) EventSyncHandler *eventSyncHandler;
+@property (nonatomic) CoreDataEventHandler *cdEventHandler;
 
 @end
 
@@ -21,13 +23,22 @@
 - (instancetype)init {
     if ((self = [super init])) {
         self.eventSyncHandler = [[EventSyncHandler alloc] init];
+        self.cdEventHandler = [[CoreDataEventHandler alloc] init];
     }
     return self;
 }
 
 - (void)deleteEvent:(nonnull Event *)event
          completion:(nonnull RemoteEventChangeCompletion)completion {
+    [self.cdEventHandler deleteEvent:event completion:^(BOOL success, NSString * _Nullable error) {
+        
+    }];
     
+    if (self.eventSyncHandler.isOnline) {
+        
+    } else {
+        
+    }
 }
 
 - (void)queryEventsOnDate:(nonnull NSDate *)date
