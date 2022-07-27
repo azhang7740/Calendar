8bfafcd2742e4d0c643f1e0cc47f23ab8f3f8d6f
Angelina Zhang <azhang7740@gmail.com>
2022-07-27 13:43:14 -0700

    Created ParseChangeHandlers to fetch changes and RevisionHistory

---
+++ CalendarApp/Change Handlers/ParseChangeHandler.h
@@ -0,0 +1,16 @@
+//
+//  ParseChangeHandler.h
+//  CalendarApp
+//
+//  Created by Angelina Zhang on 7/27/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ParseChangeHandler : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CalendarApp/Change Handlers/ParseChangeHandler.m
@@ -0,0 +1,14 @@
+//
+//  ParseChangeHandler.m
+//  CalendarApp
+//
+//  Created by Angelina Zhang on 7/27/22.
+//
+
+#import "ParseChangeHandler.h"
+#import "ParseEventBuilder.h"
+#import <CoreData/CoreData.h>
+
+@implementation ParseChangeHandler
+
+@end
