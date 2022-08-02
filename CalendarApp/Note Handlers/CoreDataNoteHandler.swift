651671315996b2aadf4d33940a9e3e96672ef23a
Angelina Zhang <azhang7740@gmail.com>
2022-08-02 15:04:40 -0700

    Implemented CoreDataNoteHandler

--- CalendarApp/Note Handlers/CoreDataNoteHandler.swift
+++ CalendarApp/Note Handlers/CoreDataNoteHandler.swift
@@ -13,30 +13,71 @@ class CoreDataNoteHandler : NSObject {
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     private let noteBuilder = CoreDataNoteBuilder()
     
-    func fetchNotes() -> [CoreDataNote] {
+    func fetchNotes() -> [Note] {
         do {
             let fetchedNotes = try context.fetch(CoreDataNote.fetchRequest())
-            return fetchedNotes
+            return noteBuilder.getNoteArray(coreDataNotes: fetchedNotes)
         } catch {
             // TODO: error handling
         }
-        return [CoreDataNote]()
+        return [Note]()
     }
     
     func deleteNote(noteID: UUID) {
+        guard let deletingNote = fetchNote(noteID: noteID) else {
+            return
+        }
+        context.delete(deletingNote)
+    }
+    
+    func update(note: Note) {
+        guard let coreDataNote = fetchNote(noteID: note.noteID) else {
+            return
+        }
+        coreDataNote.title = note.title
+        coreDataNote.createdAt = note.createdAt
+        coreDataNote.lastModified = note.lastModified
+        coreDataNote.text = note.text
+        coreDataNote.noteID = note.noteID
+        
+        do {
+            try context.save()
+        } catch {
+            // TODO: error handling
+        }
+    }
+    
+    func saveNewNote(note: Note) {
+        let newNote = CoreDataNote(context: context)
+        newNote.title = note.title
+        newNote.createdAt = note.createdAt
+        newNote.lastModified = note.lastModified
+        newNote.text = note.text
+        newNote.noteID = note.noteID
+        
+        do {
+            try context.save()
+        } catch {
+            // TODO: error handling
+        }
+    }
+    
+    private func fetchNote(noteID: UUID) -> CoreDataNote? {
         do {
             let request = CoreDataNote.fetchRequest()
             request.predicate = NSPredicate(format: "noteID == %@", noteID as CVarArg)
             let fetchedNote = try context.fetch(request)
-            if (fetchedNote.count == 1) {
-                context.delete(fetchedNote[0])
+            if fetchedNote.count == 1 {
+                return fetchedNote[0]
             }
         } catch {
             // TODO: error handling
         }
+        return nil
     }
     
-    func updateNote(note: Note) {
+    private func update(coreDataNote: CoreDataNote,
+                        note: Note) {
         
     }
 }
