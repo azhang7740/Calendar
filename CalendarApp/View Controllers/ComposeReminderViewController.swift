92c9b9acbf88520c94e6e422e67721ebe19878be
Angelina Zhang <azhang7740@gmail.com>
2022-08-05 11:01:50 -0700

    Created Compose view for reminders

---
+++ CalendarApp/View Controllers/ComposeReminderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ComposeReminderViewController.swift
+//  CalendarApp
+//
+//  Created by Angelina Zhang on 8/5/22.
+//
+
+import Foundation
+
+class ComposeReminderViewController: UIViewController {
+    public var reminder: Reminder?
+    private let notificationHandler = NotificationHandler()
+    
+    
+}
